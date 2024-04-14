2788b864e0ba59c88d48d298fae9b33f2bb4bdec
ohjmi <ohjmi@naver.com>
2024-04-14 22:11:43 +0900

    - 타입스크립트 설치 - 타입스크립트 옵션 설정

---
+++ section1/dist/index.js
@@ -0,0 +1,7 @@
+// console.log("Hello Typescript");
+// const a: number = 1;
+// const func = () => console.log("Hello");
+// import { hello } from "./hello";
+// hello();
+const a = 1;
+export {};
---
+++ section1/src/index.ts
@@ -0,0 +1,10 @@
+// console.log("Hello Typescript");
+// const a: number = 1;
+
+// const func = () => console.log("Hello");
+
+// import { hello } from "./hello";
+
+// hello();
+
+const a = 1;
